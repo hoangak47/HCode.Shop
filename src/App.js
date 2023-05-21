9690415f55e93d30b18cd2e8bd33832961079e38
hoangak47 <hoang41911@gmail.com>
2023-05-21 23:38:53 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
-import publicRoutes from './config/routes';
-import DefaultLayout from './Layout/DefaultLayout';
-import { Fragment } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
     return (
-        <>
-            <Routes>
-                {publicRoutes.map((route, index) => {
-                    const Page = route.component;
-                    let Layout = DefaultLayout;
-
-                    if (route.layout) {
-                        Layout = route.layout;
-                    } else if (route.layout === null) {
-                        Layout = Fragment;
-                    }
-
-                    return (
-                        <Route
-                            key={index}
-                            path={route.path}
-                            exact={route.exact ? route.exact : false}
-                            element={<Layout>{<Page />}</Layout>}
-                        />
-                    );
-                })}
-            </Routes>
-        </>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import GlobalStyle from './Component/GlobalStyle';
-import store from './app/store';
-import { Provider } from 'react-redux';
-
-import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <Router>
-                <GlobalStyle>
-                    <App />
-                </GlobalStyle>
-            </Router>
-        </Provider>
+        <App />
     </React.StrictMode>,
 );
 
