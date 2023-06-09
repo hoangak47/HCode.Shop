34c6847bd1e1ade46a9cae189e74e9f6df910885
hoangak47 <hoang41911@gmail.com>
2023-06-09 21:52:03 +0700

    q

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,2 @@
-export const api = 'http://localhost:5000';
-// export const api = 'https://hcode-shop-api.vercel.app';
+// export const api = 'http://localhost:5000';
+export const api = 'https://hcode-shop-api.vercel.app';
